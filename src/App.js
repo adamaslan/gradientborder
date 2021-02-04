01698a820bea3d00550682da23417e652148f965
Adam Aslan <chillcoders@gmail.com>
2021-02-03 19:56:58 -0500

    gradient1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Mixer from './mixer.gif';
+import './gradientborder.css'; 
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div class="rainbow">
         <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div class="rainbow">
+        <img 
+          src={Mixer}
+        />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
